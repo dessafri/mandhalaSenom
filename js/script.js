95151e64458ba810f703752b0f40a658c7e83056
dessafri <41738535+dessafri@users.noreply.github.com>
2021-10-20 17:51:40 +0700

    add jumbotron dan slick libraries for carousel

---
+++ js/script.js
@@ -0,0 +1,14 @@
+new Typewriter('#typewriter', {
+  strings: ['Kampung Pendidikan Mandhala Senom'],
+  autoStart: true,
+  loop: true,
+  pauseFor: 1500,
+})
+
+$('.image-animate').slick({
+  autoplay: true,
+  arrows: false,
+  easing: 'swing',
+  autoplaySpeed: 2000,
+  infinite: true,
+})
