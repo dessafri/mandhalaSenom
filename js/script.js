eb2f4fe4c0fa99f51499ee8fafc221d371ed0c0a
dessafri <41738535+dessafri@users.noreply.github.com>
2021-10-21 11:54:44 +0700

    add map, add profile

--- js/script.js
+++ js/script.js
@@ -12,3 +12,23 @@ $('.image-animate').slick({
   autoplaySpeed: 2000,
   infinite: true,
 })
+let map = () => {
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoiZGVzc2FmcmkiLCJhIjoiY2t1emVlNjh6MDN2NTJ2cGF6d2pveHdrcCJ9.Mw1HrmGLNs7rn0OHjIgNhA'
+  let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+  })
+  const marker = new mapboxgl.Marker({
+    color: '#fc0303',
+  })
+    .setLngLat([113.4875230501, -7.167193637305])
+    .addTo(map)
+  map.setCenter([113.4875230501, -7.167193637305])
+  map.zoomTo(1)
+  //   // Zoom to the zoom level 8 with an animated transition
+  map.zoomTo(17, {
+    duration: 9000,
+  })
+}
+map()
